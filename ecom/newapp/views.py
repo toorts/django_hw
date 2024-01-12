94e1ef8f2144605618def32e380e384519d430ef
Martin House <torchester@gmail.com>
2024-01-12 19:13:30 +0500

    first commit

--- ecom/newapp/views.py
+++ ecom/newapp/views.py
@@ -4,17 +4,19 @@ import logging
 # Получение экземпляра логгера
 logger = logging.getLogger(__name__)
 
+
 def home(request):
     # HTML-вёрстка для главной страницы
     html_content = """
         <h1>Главная страница Django-сайта.</h1>
         <p>Рады видеть Вас в нашем интернет-магазине</p>
     """
-    
+
     # Логирование посещения страницы
     logger.info("home get request")
-    
+
     return HttpResponse(html_content)
+
 
 def about(request):
     # HTML-вёрстка для страницы "о нас"
@@ -22,8 +24,8 @@ def about(request):
         <h1>О нас</h1>
         <p>Привет! Меня зовут Django и это мой интернет-магазине.</p>
     """
-    
+
     # Логирование посещения страницы
     logger.info("about get request")
-    
+
     return HttpResponse(html_content)
